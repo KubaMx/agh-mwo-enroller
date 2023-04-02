19f5b1446ec0401208fdc9afd3fc6311c62e6771
student18 <student18@pcoip.ki.agh.edu.pl>
2023-04-02 12:19:26 +0200

    adding files after training session

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,10 +2,17 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
+
+import javax.servlet.http.Part;
+
+import static com.company.enroller.persistence.HibernateUtil.*;
 
 @Component("participantService")
 public class ParticipantService {
@@ -22,4 +29,35 @@ public class ParticipantService {
 		return query.list();
 	}
 
+    public Participant findByLogin(String login) {
+		String hql = "FROM Participant where login = :login";
+		Query query = connector.getSession().createQuery(hql);
+		query.setParameter("login", login);
+		try {
+			return (Participant) query.getSingleResult();
+		} catch (Exception e){
+			return null;
+		}
+    }
+
+	public Participant register(Participant participant){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+		return participant;
+	}
+
+	public Participant delete(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+		return participant;
+	}
+
+	public Participant update(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().update(participant);
+		transaction.commit();
+		return participant;
+	}
 }
